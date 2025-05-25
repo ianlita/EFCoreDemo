65887c8aeb43da2a96d5121126d2546eb0b002d5
xfinity <148526177+ianlita@users.noreply.github.com>
2025-05-25 18:45:55 +0800

    add invoice item configration and implement its properties and relationship. Declare new controller (for InvoiceItem)

---
+++ EFCore/Migrations/20250525095651_AddInvoiceConfig.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvoiceConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Invoices",
+                keyColumn: "InvoiceId",
+                keyValue: new Guid("36de8af9-349d-4864-bd7e-9548f5b04e44"));
+
+            migrationBuilder.DeleteData(
+                table: "Invoices",
+                keyColumn: "InvoiceId",
+                keyValue: new Guid("d03342e9-cda2-4838-9fbe-98f2cd0ad2cb"));
+
+            migrationBuilder.DeleteData(
+                table: "Invoices",
+                keyColumn: "InvoiceId",
+                keyValue: new Guid("ffb2456b-14bb-4574-ba69-e2d3067f59cc"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Invoices",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                table: "Invoices",
+                type: "varchar(32)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Invoices",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactName",
+                table: "Invoices",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Invoices",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactName",
+                table: "Invoices",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.InsertData(
+                table: "Invoices",
+                columns: new[] { "InvoiceId", "Amount", "ContactName", "Description", "DueDate", "InvoiceDate", "InvoiceNumber", "Status" },
+                values: new object[,]
+                {
+                    { new Guid("36de8af9-349d-4864-bd7e-9548f5b04e44"), 300m, "Thor", "Invoice for the first month", new DateTimeOffset(new DateTime(2021, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "INV-003", 0 },
+                    { new Guid("d03342e9-cda2-4838-9fbe-98f2cd0ad2cb"), 100m, "Iron Man", "Invoice for the first month", new DateTimeOffset(new DateTime(2021, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "INV-001", 5 },
+                    { new Guid("ffb2456b-14bb-4574-ba69-e2d3067f59cc"), 200m, "Captain America", "Invoice for the first month", new DateTimeOffset(new DateTime(2021, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "INV-002", 5 }
+                });
+        }
+    }
+}
